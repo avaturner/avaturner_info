386a99745faf5c2416cb3ba5e477cda8da9f27a1
Michael B Hand <michael.hand@everydayhealthinc.com>
2023-07-27 14:48:15 -0400

    Title: Ticket: Message: Set up contyent edit local storage Note:

--- webpack-setup/src/script.js
+++ webpack-setup/src/script.js
@@ -16,7 +16,8 @@ import {
         {
           name: 'resume',
           title: 'Resume',
-          text: '- Click Here for my Resume '
+          text: '- Click Here for my Resume ',
+          href: 'AvaTurnerResume.pdf'
         },
         {
           name: 'accomplishments',
@@ -30,22 +31,30 @@ import {
         }
       ];
 
-      const local_content = localStorage.getItem('local_content') || [];
+      const local_storage_content = JSON.parse(localStorage.getItem('local_content')) || [];
+
+      // const local_storage_content = JSON.parse(localStorage.getItem('local_content'));
+
+
+
+
+      this.edit_mode = /changes_form/.test(document.location.pathname);
 
       Object.assign(this, {
-        content: [
-          ...default_content,
-          ...local_content,
-        ],
-        main_section: document.querySelector('main'),
+        content: local_storage_content.length ? local_storage_content : default_content,
+        primary_section: document.querySelector('.primary'),
+        main_section: document.querySelector(this.edit_mode ? '.add_changes' : '.main_content'),
         main_element: document.querySelector('main'),
         footer_object: document.querySelector('footer'),
+        changeB: document.querySelector('.add_changes'),
 
       }, config);
 
-      localStorage.setItem('first_p', JSON.stringify(this.first_p));
-      localStorage.setItem('second_p', JSON.stringify(this.second_p));
-      localStorage.setItem('third_p', JSON.stringify(this.third_p));
+
+      console.warn('>> local_storage_content: ', this.content);
+      // localStorage.setItem('first_p', JSON.stringify(this.first_p));
+      // localStorage.setItem('second_p', JSON.stringify(this.second_p));
+      // localStorage.setItem('third_p', JSON.stringify(this.third_p));
 
       this.content_text = this.content;
       if (this.footer_object != null) {
@@ -54,8 +63,12 @@ import {
     }
 
     init() {
-      if (this.main_section != null && !/changes_form/.test(document.location.pathname)) {
+      this.update_nav();
+      if (this.main_section != null && !this.edit_mode) {
         this.content_text.forEach(section => {
+          if (section.href) {
+            return;
+          }
           const new_section = document.createElement('section');
           const new_section_element_text = document.createElement('div');
           const new_section_element_title = document.createElement('div');
@@ -73,74 +86,105 @@ import {
           this.main_section.insertAdjacentElement('beforeend', new_section);
           this.main_section.insertAdjacentElement('beforeend', form_page_element);
         });
-        this.update_nav();
+
       }
-      else if (this.main_section != null && /changes_form/.test(document.location.pathname)) {
+      else if (this.main_section != null && this.edit_mode) {
+        console.warn('>> this.main_section: ', this.main_section);
         this.insert_form();
+        this.content_pulldown_gen();
+        this.event_listeners();
       }
       this.footer();
-      this.event_listeners();
     }
 
 
 
     event_listeners() {
-      this.changeB = document.querySelector('.add_change_button');
-      if (this.changeB != null) {
-        this.changeB.addEventListener("click", () => {
-          this.insert_form();
+      this.save_button = document.querySelector('.save_button');
+      if (this.save_button != null) {
+        this.save_button.addEventListener("click", (e) => {
+
+          this.content[this.selected_index] = { title: this.new_title.value, name: this.new_name.value, text: this.new_note_area.value }
+          console.warn('>> selected_index <<', this.content[this.selected_index]);
+          localStorage.setItem('local_content', JSON.stringify(this.content));
         });
       }
+
+      this.new_delete_button.addEventListener("click", (e) => {
+        console.warn('>> Click Delete <<');
+        //same as above, just deleted from local storage, calls method
+        // const _id = e.target.dataset.id;
+        // this.delete_notes({
+        //   id: _id,
+        //   element: e.target
+        // });
+      })
+    }
+
+    content_pulldown_gen() {
+      this.pulldown_menu_container = document.createElement('div');
+      this.pulldown_menu_container.classList.add('pulldown_menu_container');
+      this.pulldown_menu = document.createElement('select');
+      for (let index = 0; index < this.content.length; index++) {
+        const element = this.content[index];
+        this.pulldown_menu.insertAdjacentHTML('beforeend', `<option value="${index}" data-id="${index}">${element.title}</option>`)
+      }
+      this.pulldown_menu.addEventListener('change', (e) => {
+        console.warn('pulldown element: ', this.pulldown_menu[e.target.selectedIndex].dataset.id);
+        const selected_option = this.content[e.target.value];
+        this.new_title.value = selected_option.title;
+        this.new_note_area.value = selected_option.text;
+        this.new_name.value = selected_option.name;
+        this.selected_index = this.pulldown_menu[e.target.selectedIndex].dataset.id;
+      });
+
+      this.pulldown_menu_container.insertAdjacentElement('afterbegin', this.pulldown_menu);
+      this.new_note_container.insertAdjacentElement('afterbegin', this.pulldown_menu_container);
     }
 
     insert_form({ title, note } = {}) {
-      const new_note_container = document.createElement("div"); //whole note section
-      const new_title = document.createElement("input"); //first box
-      const new_note = document.createElement("textarea"); //second box
+      console.warn('>> insert_form <<');
+      this.note_id = 1;
+      this.new_note_container = document.createElement("div"); //whole note section
+      this.new_note_container.classList.add('note_container');
+      this.new_title = document.createElement("input"); //first box
+      this.new_name = document.createElement("input"); //first box
+      const new_title_container = document.createElement("div"); //first box
+      const new_name_container = document.createElement("div");
+      this.new_note_area = document.createElement("textarea"); //second box
+      this.new_note_area_container = document.createElement("div"); //second box
+      this.new_note_area_container.classList.add('new_note_area_container');
       const new_save_button = document.createElement("div"); //save button
-      const new_delete_button = document.createElement("div"); //delete button
-      new_delete_button.classList.add("delete_button"); //explain classList?
+      this.new_delete_button = document.createElement("div"); //delete button
+      this.new_delete_button.classList.add("delete_button"); //explain classList?
       new_save_button.classList.add("save_button");
       new_save_button.dataset.id = this.note_id; //gives each note an ID
       new_save_button.innerText = "save";
-      new_title.type = "text";
-      new_title.dataset.id = this.note_id;
-      new_note.dataset.id = this.note_id;
-      new_delete_button.dataset.id = this.note_id;
+      this.new_title.type = "text";
+      this.new_name.type = "text";
+      new_title_container.classList.add('new_title');
+      new_name_container.classList.add('new_name');
+      this.new_note_area.dataset.id = this.note_id;
+      this.new_delete_button.dataset.id = this.note_id;
       //buttons build bottom up, inserted onto page
-      new_note_container.insertAdjacentElement("afterbegin", new_save_button);
-      new_note_container.insertAdjacentElement("afterbegin", new_note);
-      new_note_container.insertAdjacentElement("afterbegin", new_title);
-      new_note_container.insertAdjacentElement("afterbegin", new_delete_button);
-      this.blogB.insertAdjacentElement("beforebegin", new_note_container); //where it is put on the page
-      new_note_container.classList.add("note_container");
+      this.new_note_container.insertAdjacentElement("afterbegin", new_save_button);
+      this.new_note_area_container.insertAdjacentElement("afterbegin", this.new_note_area);
+      this.new_note_container.insertAdjacentElement("afterbegin", this.new_note_area_container);
 
-      new_save_button.addEventListener("click", (e) => {
-        //if save button is clicked, add title and note value is added to that ID number
-        const _id = e.target.dataset.id;
-        this.notes_object[_id] = {
-          title: new_title.value,
-          note: new_note.value
-        };
-        //console.warn(">> notes_object: ", this.notes_object);
-        //that notes ID object is added to the localStorage
-        //converted to JSON string file
-        //notes is name of object it is going into
-        localStorage.setItem('notes', JSON.stringify(this.notes_object));
-      })
+      new_title_container.insertAdjacentElement("afterbegin", this.new_title);
+      this.new_note_container.insertAdjacentElement("afterbegin", new_title_container);
 
-      new_delete_button.addEventListener("click", (e) => {
-        //same as above, just deleted from local storage, calls method
-        const _id = e.target.dataset.id;
-        this.delete_notes({
-          id: _id,
-          element: e.target
-        });
-      })
+      new_name_container.insertAdjacentElement("afterbegin", this.new_name);
+      this.new_note_container.insertAdjacentElement("afterbegin", new_name_container);
 
-      this.note_id += 1; //increments id for each added note
-      new_title.value = title || '';
-      new_note.value = note || '';
+      this.new_note_container.insertAdjacentElement("afterbegin", this.new_delete_button);
+      this.changeB.insertAdjacentElement("beforebegin", this.new_note_container); //where it is put on the page
+      this.new_note_container.classList.add("note_container");
+
+      // this.note_id += 1; //increments id for each added note
+      // this.new_title.value = title || '';
+      // this.new_note_area.value = note || '';
+      this.main_section.insertAdjacentElement("afterbegin", this.new_note_container); //where it is put on the page
     }
 
     update_nav() {
@@ -148,9 +192,9 @@ import {
 
       this.content.forEach(element => {
         const nav_item = document.createElement('a');
-        nav_item.href = element.href ? element.href : `#${element.title}`;
+        nav_item.href = element.href ? element.href : `#${element.name}`;
         nav_item.target = element.href ? '_blank' : '';
-        nav_item.classList.add(`special_nav_${element.title}`);
+        nav_item.classList.add(`special_nav_${element.name}`);
         nav_item.innerText = element.title;
         nav_container.insertAdjacentElement('afterbegin', nav_item);
       });
@@ -166,7 +210,7 @@ import {
       //     nav_container.insertAdjacentElement('afterbegin', nav_item);
       //   }
       // }
-      this.main_section.insertAdjacentElement('afterbegin', nav_container);
+      this.primary_section.insertAdjacentElement('afterbegin', nav_container);
     }
 
     footer() {
